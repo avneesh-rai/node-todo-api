e6ae7dd6d579c08566cae3aaef8d1f206e93bd1a
Avneesh Rai <avneesh.rai@jda.com>
2019-02-02 18:40:05 +0530

    Test POST /todos route

---
+++ server/tests/server.test.js
@@ -0,0 +1,60 @@
+const expect = require('expect');
+const request = require('supertest');
+
+var {app} = require('./../server');
+var {Todo} = require('./../models/todo');
+
+beforeEach((done) => {
+  Todo.deleteMany().then(() => {
+      done();
+  });
+});
+
+describe('Post /todos', () => {
+  it('should create a new todo', (done) => {
+    var text = 'Test todo text';
+
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(200)
+      .expect((response) => {
+        expect(response.body.text).toBe(text);
+      })
+      .end((err, response) => {
+        if (err) {
+          return done(err);
+        }
+        Todo.find().then((todos) => {
+          expect(todos.length).toBe(1);
+          expect(todos[0].text).toBe(text);
+          done();
+        }).catch((err) => {
+          done(err);
+        });
+      });
+  });
+
+  it('should throw exception', (done) => {
+    var text = "";
+    request(app)
+      .post('/todos')
+      .send({text})
+      .expect(400)
+      // .expect((response) => {
+      //   expect(response.body).toBe(text);
+      // })
+      .end((err, response) => {
+        if(err){
+          return done(err);
+        }
+        Todo.find().then((todos)=>{
+          expect(todos.length).toBe(0);
+          // expect(todos[0].text).toBe(text);
+          done();
+        }).catch((e)=>{
+          done(e);
+        });
+      });
+  });
+});
