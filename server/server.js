2574bc8c448b258408364af69ccec8bf67ff85fe
Avneesh Rai <avneesh.rai@jda.com>
2019-02-03 13:55:34 +0530

    Added PATCH/todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectId} = require('mongodb');
@@ -57,6 +58,34 @@ app.delete('/todos/:id', (request, response) => {
     if(!todo){
       return response.status(404).send();
     }
+    response.send({todo});
+  }).catch((e) => {
+    response.status(400).send();
+  });
+});
+
+app.patch('/todos/:id', (request, response) => {
+  var id = request.params.id;
+  var body = _.pick(request.body,['text','completed']);
+  if(!ObjectId.isValid(id)) {
+    return response.status(404).send();
+  }
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = 'false';
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id,
+    {
+      $set: body
+    }, {
+      new: true
+    }).then((todo)=>{
+      if(!todo){
+        response.status(404).send();
+      }
     response.send({todo});
   }).catch((e) => {
     response.status(400).send();
