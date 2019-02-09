282dae4aa4e1595dc4284f91fed6931af408f4d8
Avneesh Rai <avneesh.rai@jda.com>
2019-02-09 12:58:56 +0530

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,11 +1,12 @@
 var env = process.env.NODE_ENV || 'development';
 
-if (env === 'production') {
-  process.env.MONGODB_URI = 'mongodb://avi:avi123@ds119795.mlab.com:19795/todoapp-avi'
-} else if (env === 'development') {
-  process.env.port = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp'
-} else if (env === 'test') {
-  process.env.port = 3000;
-  process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest'
+
+if (env === 'test' || env === 'development' || env === 'production') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+      process.env[key] = envConfig[key];
+    });
+
 }
