b0ca53d079317d40ea5fb093b04615386e9d574f
Avneesh Rai <avneesh.rai@jda.com>
2019-02-09 13:27:37 +0530

    Removing production from config

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,26 @@
 var env = process.env.NODE_ENV || 'development';
 
 
-if (env === 'test' || env === 'development' || env === 'production') {
+if (env === 'test' || env === 'development') {
     var config = require('./config.json');
     var envConfig = config[env];
 
     Object.keys(envConfig).forEach((key) => {
       process.env[key] = envConfig[key];
     });
-
 }
+
+
+// if (env === 'test' || env === 'development' || env === 'production') {
+//     var config = require('./config.json');
+//     var envConfig = config[env];
+//
+//     Object.keys(envConfig).forEach((key) => {
+//       process.env[key] = envConfig[key];
+//     });
+// }
+
+// Set MONGODB_URI using heroku config:set MONGODB_URI=mongodb://avi:avi123@ds119795.mlab.com:19795/todoapp-avi
+// "production": {
+//   "MONGODB_URI": "mongodb://avi:avi123@ds119795.mlab.com:19795/todoapp-avi"
+// }
